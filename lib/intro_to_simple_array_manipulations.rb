ed3474b9d61ecef05dc96d62697f4ca9c91f6755
nickie kane <kameo712@gmail.com>
2019-12-25 23:27:17 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -72,8 +72,8 @@ end
 
 using_delete(animals, "Cat")
 
-def using_delete_at(animals, animal)
-animals.delete(animal)
+def using_delete_at(animals, num)
+animals.delete(num)
 end
 
 using_delete_at(animals, 1)
