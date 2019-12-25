e653c877c34438de33d0c76a7595ada0fc0b7205
nickie kane <kameo712@gmail.com>
2019-12-25 23:17:34 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,51 @@
+animals = ["Dog", "Horse", "Seal"]
+new_animal = "Cat"
+
+def using_push(animals, new_animal)
+animals.push(new_animal)
+end
+
+using_push(animals, new_animal)
+
+def using_unshift(animals, new_animal)
+animals.unshift(new_animal)
+end
+
+using_unshift(animals, new_animal)
+
+def using_pop(animals)
+animals.pop
+end
+
+using_pop(animals)
+
+def pop_with_args(animals)
+animals.pop(2)
+end
+
+pop_with_args(animals)
+
+def using_shift(animals)
+animals.shift
+end
+
+using_shift(animals)
+
+def shift_with_args(animals)
+animals.shift(2)
+end
+
+shift_with_args(animals)
+
+more_animals = ["lion", "tiger", "leopard"]
+def using_concat(animals, more_animals)
+animals.concat(more_animals)
+end
+
+using_concat(animals, more_animals)
+
+def using_insert(animals, animal)
+animals.insert(4, animal)
+end
+
+using_insert(animals, "turtle")
